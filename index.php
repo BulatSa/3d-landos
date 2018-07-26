55dfb4e5b6a1eabb1aeb7839132768b0b5cd6f24
Bulat <sadikovba@gmail.com>
2018-07-26 14:15:34 +0300

    Initial

---
+++ index.php
@@ -0,0 +1,33 @@
+﻿<?php include('header.php'); ?>
+
+<section class="header-sec">
+	<div class="header container">
+		<div class="header__logo">
+			<a href="#">
+				<img src="img/logo-top.svg" alt="">
+			</a>
+		</div>
+		<div class="header__about">
+			<div class="header__about-img">
+				<img src="img/header/logo-about.png" alt="">
+			</div>
+			<span>Официальный дистрибьютор</span>
+		</div>
+		<div class="header__garant">
+			<img src="img/header/icon-1.png" alt="">
+			<div class="header__garant-info">
+				<p>Гарантия 2 года</p>
+				<span>Гарантия от производителя</span>
+			</div>
+		</div>
+		<div class="header__contact">
+			<img src="img/header/icon-2.png" alt="">
+			<div class="header__contact-info">
+				<a href="tel:8 800 600-17-10">8 800 600-17-10</a>
+				<a href="#" class="fancy">Обратный звонок</a>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php include('footer.php'); ?>
