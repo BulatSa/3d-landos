076725a2879db3b303888bb452600d5fe7cf4a1e
Andrew <andreiduffy@gmail.com>
2018-07-26 16:04:43 +0300

    слайдеры и преимущества

--- index.php
+++ index.php
@@ -30,4 +30,122 @@
 	</div>
 </section>
 
+
+<section class="s-about">
+	<div class="container">
+		<div class="row">
+
+			<div class="grid-5 grid-12_s">
+				<p class="h1 s-about__title">3D принетре Picaso 3D Designer X Pro</p>
+				<div class="user-content">
+					<p>Моментальная смена материалов – печатающая головка JetSwitch при работе в два сопла стала еще быстрее. Смена филамента теперь происходит от 250 мс до 5 с в зависимости от профиля печати.</p>
+					<p>Эталонное качество – при смене материала точность позиционирования сопел достигает 1 микрона, жесткая корпусная рама, модернизированная механика и аппаратная платформа PICASO X.</p>
+					<p>Контроль состояния – функция, позволяющая осуществлять мониторинг за бесперебойностью подачи пластиковой нити в экструдер, состоянием сопла, тем как печатается первый слой и информировать оператора о реальном состоянии принтера.</p>
+					<p>Системный подход к калибровке - автоматическая калибровка рабочей платформы, автоматическая калибровка высоты сопел, калибровка смещения сопел.</p>
+				</div>
+			</div>
+
+			<div class="grid-7 grid-12_s">
+				<div class="slider">
+					<div class="slide">
+						<a href="/img/slide_1.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb.png" data-flickity-lazyload="/img/slide_1.jpg" alt="1">
+						</a>
+					</div>
+					<div class="slide">
+						<a href="/img/slide_2.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb.png" data-flickity-lazyload="/img/slide_2.jpg" alt="1">
+						</a>
+					</div>
+					<div class="slide">
+						<a href="/img/slide_1.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb.png" data-flickity-lazyload="/img/slide_1.jpg" alt="1">
+						</a>
+					</div>
+				</div>
+			</div>
+
+		</div>	
+	</div>
+</section>
+
+
+<section class="s-result">
+	<div class="container">
+		<div class="row">
+
+			<div class="grid-5 grid-12_s">
+				<p class="h1 s-result__title">Результат говорит сам за себя</p>
+				<div class="user-content">
+					<p>В 3D принтере Designer X PRO применяются рельсовые направляющие. Они повышают скорость перемещения экструдера и точность его позиционирования. Использование стальной рамы улучшает жесткость конструкции</p>
+					<p>Технология переключения сопел – JetSwitch, ускоряет процесс создания изделий и экономит ваше время. В качестве эксперимента была отпечатана модель сверла высотой 18 м. Обычный принтер с двумя экструдерами выполнил задание за 37 часов, в то время как Designer X PRO изготовил эту же деталь за 22 часа. И все это благодаря тому, что JetSwitch позволяет производить переключение между материалами происходит за 5 секунд.</p>
+				</div>
+			</div>
+
+			<div class="grid-7 grid-12_s">
+				<div class="slider">
+					<div class="slide">
+						<a href="/img/slide_2.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb-dark.png" data-flickity-lazyload="/img/slide_2.jpg" alt="1">
+						</a>
+					</div>
+					<div class="slide">
+						<a href="/img/slide_1.jpg" class="fancy" data-fancybox="slider_about">
+							<img src="/img/thumb-dark.png" data-flickity-lazyload="/img/slide_1.jpg" alt="1">
+						</a>
+					</div>
+				</div>
+			</div>
+
+		</div>	
+	</div>
+</section>
+
+
+<section class="s-preims">
+	<div class="container">
+		<p class="h2 s-preims__title">Преимущества сотрудничества</p>
+
+		<div class="preims">
+			<div class="preim">
+				<header>
+					<img src="/img/preims/1.jpg" alt="1">
+				</header>
+				<div class="preim__title">Бесплатная доставка</div>
+				<div class="preim__descr">осуществляется по&nbsp;РФ до&nbsp;ближайшего к&nbsp;покупателю пункту выдачи транспортной компании.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/2.jpg" alt="1">
+				</header>
+				<div class="preim__title">Специальные условия</div>
+				<div class="preim__descr">На поставки для&nbsp;образовательных организаций.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/3.jpg" alt="1">
+				</header>
+				<div class="preim__title">Обучение в&nbsp;подарок</div>
+				<div class="preim__descr">Обучаем вашего специалиста в&nbsp;офисах компании (Казань и&nbsp;Санкт-Петербург) при&nbsp;покупке оборудования.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/4.jpg" alt="1">
+				</header>
+				<div class="preim__title">Пусконаладка в&nbsp;подарок</div>
+				<div class="preim__descr">Осуществление бесплатных пусконаладочных работ в&nbsp;городах Казань и&nbsp;Санкт-Петербург.</div>
+			</div>
+			<div class="preim">
+				<header>
+					<img src="/img/preims/5.jpg" alt="1">
+				</header>
+				<div class="preim__title">Постоянная скидка до&nbsp;20%</div>
+				<div class="preim__descr">Обучаем вашего специалиста в&nbsp;офисах компании (Казань и Санкт-Петербург) при&nbsp;покупке оборудования.</div>
+			</div>
+		</div>
+
+	</div>
+</section>
+
+
 <?php include('footer.php'); ?>
--- js/main.js
+++ js/main.js
@@ -156,3 +156,25 @@ $(function () {
 /***********************
  Waypoints END
  ***********************/
+
+
+/***********************
+Sliders BEGIN
+***********************/
+$(function($){
+	$('.slider').flickity({
+		cellAlign: 'center',
+		contain: true,
+		pageDots: false,
+		lazyLoad: true,
+		arrowShape: { 
+			x0: 10,
+			x1: 60, y1: 50,
+			x2: 65, y2: 45,
+			x3: 20
+		 }
+	 });
+})
+/***********************
+Sliders END
+***********************/
